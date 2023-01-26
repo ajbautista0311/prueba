c6fb67d974943c22b66c48c4a457f3427ca3d565
Agente G <ajbautista0311@gmail.com>
2023-01-26 09:06:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-variable "prueba"{
-  type=string
-  default="Esta es mi primer variable de terraform"
+variable "pruebamap"{
+  type=map(string)
+  default={'1': 'Monday', '2': 'Tuesday', '3': 'Monday'}
+}
+
+variable "pruebalist"{
+  type=list
+  default=["a", 15, true]
 }
